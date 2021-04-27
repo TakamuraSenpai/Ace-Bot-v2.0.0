db4ffeb00b506a4f72b67db9be783b66e477f4a4
よでとげせTakamura <60247685+TakamuraSenpai@users.noreply.github.com>
2021-04-27 09:08:33 -0300

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,274 @@
+const help = (prefix) => {
+	return ` 𝑩𝑶𝑻 𝑾𝑯𝑨𝑻𝑺𝑨𝑷𝑷 𝑻𝑬𝑹𝑴𝑼𝑿
+Salve Meu Consagrado Muito Obrigrado Ultilizar O Bot😁
+
+𝑰𝑵𝑭𝑶𝑹𝑴𝑨𝑪̧𝑶̃𝑬𝑺 𝑺𝑶𝑩𝑹𝑬 𝑶 𝑩𝑶𝑻:
+
+STATUS DO BOT: ᴏɴʟɪɴᴇ ●ω●
+
+Bot Criado Por: Takamura
+Verçao:2.0.0
+
+          CRIADO VIA: GIT HUB/TERMUX
+
+DONO: wa.me/+558695023910
+Chame ele Em Caso De BUG ou Ideias Para BOT
+
+  𝘼𝙌𝙐𝙄 𝙑𝘼̃𝙊 𝙀𝙎𝙏𝘼́ 𝘼𝙇𝙂𝙐𝙉𝙎 𝘾𝙊𝙈𝘼𝙉𝘿𝙊𝙎:
+
+●ω●【𝗠𝗘𝗟𝗛𝗢𝗥𝗘𝗦】
+
+🔥 *${prefix}Sticker* [Faz figurinha]
+🔥 *${prefix}play* [nome da música]
+🔥 *${prefix}toimg* [converter figurinha em imagem]
+🔥 *${prefix}wame* [link do seu whatsapp]
+🔥 *${prefix}meme* [memes aleatórios]
+🔥 *${prefix}nabutojokes* [memes2]
+🔥 *${prefix}tts pt* [seu texto]
+🔥 *${prefix}ping* [velocidade]
+🔥 *${prefix}owner ou dono* [info do criador]
+
+●ω●【𝗡𝗢𝗩𝗢𝗦】
+
+🔥 *${prefix}animecry*
+🔥 *${prefix}chentai [premium]*
+🔥 *${prefix}gcpf [premium]*
+🔥 *${prefix}gay [@]*
+🔥 *${prefix}gbin [premium]*
+🔥 *${prefix}pack [premium]*
+🔥 *${prefix}destrava [premium]*
+🔥 *${prefix}gpessoa [premium]*
+🔥 *${prefix}spamcall*
+🔥 *${prefix}play (nome da msc)*
+
+●ω●【𝗣𝗔𝗥𝗔 𝗚𝗥𝗨𝗣𝗢𝗦】
+
+🔥 *${prefix}closegc* [fechar grupo]
+🔥 *${prefix}opengc* [abrir grupo]
+🔥 *${prefix}antilink* 1 [anti link]
+🔥 *${prefix}antiracismo on* [anti racismo]
+🔥 *${prefix}banir* [banir membro]
+🔥 *${prefix}admins* [lista se administradores]
+🔥 *${prefix}marcar* [marcar todos membros]
+🔥 *${prefix}linkgp* [link do grupo]
+🔥 *${prefix}promover* [dar adm]
+🔥 *${prefix}rebaixar* [tirar adm]
+🔥 *${prefix}bemvindo* 1 [recusso de boas vindas]
+🔥 *${prefix}grupoinfo* [info]
+🔥 *${prefix}setdesc* [trocar descrição]
+🔥 *${prefix}setfoto* [mudar foto]
+🔥 *${prefix}porno* [porno]
+🔥 *${prefix}mia* [fotos da mia]
+
+●ω●【𝗜𝗡𝗧𝗘𝗥𝗔𝗚𝗜𝗥】
+➛
+🔥 *${prefix}figu*
+🔥 *${prefix}toimg*
+🔥 *${prefix}nabutojokes (memes aleatórios)*
+🔥 *${prefix}memeindo*
+🔥 *${prefix}tts*
+🔥 *${prefix}lolih [on]*
+🔥 *${prefix}nsfwloli [off]*
+🔥 *${prefix}url2img*
+🔥 *${prefix}leens [na legenda]*
+🔥 *${prefix}wait [na legenda]*
+🔥 *${prefix}setprefix*
+➛
+●ω●【𝗢𝗨𝗧𝗥𝗢𝗦】
+➛
+🔥 *${prefix}linkgp*
+🔥 *${prefix}simih [1/0]*
+🔥 *${prefix}marcar*
+🔥 *${prefix}add [@]*
+🔥 *${prefix}banir [@]*
+🔥 *${prefix}promover [@]*
+🔥 *${prefix}rebaixar*
+🔥 *${prefix}admins*
+🔥 *${prefix}marcar2*
+🔥 *${prefix}bc [texto]* (ele faz uma ™)
+🔥 *${prefix}marcar3*
+🔥 *${prefix}bloqueados*
+🔥 *${prefix}bloquear [@]*
+🔥 *${prefix}desbloquear [@]*
+🔥 *${prefix}limpar*
+🔥 *${prefix}bc [ *texto* ]*
+🔥 *${prefix}bemvindo [1/0]*
+🔥 *${prefix}clonar [Removido]*
+🔥 *${prefix}help1*
+🔥 *${prefix}dono*
+🔥 *${prefix}owner*
+🔥 *${prefix}tts [texto]*
+🔥 *${prefix}setnome*
+🔥 *${prefix}termux*
+🔥 *${prefix}setfoto*
+🔥 *${prefix}grupoinfo*
+🔥 *${prefix}ytmp4*
+🔥 *${prefix}bomdia*
+🔥 *${prefix}boanoite*
+🔥 *${prefix}marcar*
+🔥 *${prefix}marcar2*
+🔥 *${prefix}marcar3*
+➛
+●ω●【 𝗜𝗠𝗔𝗚𝗘𝗡𝗦 】
+➛
+🔥 *${prefix}loli* [off]
+🔥 *${prefix}loli1*
+🔥 *${prefix}hentai*
+🔥 *${prefix}dono*
+🔥 *${prefix}porno*
+🔥 *${prefix}boanoite*
+🔥 *${prefix}bomdia*
+🔥 *${prefix}boatarde*
+🔥 *${prefix}mia [aleatórias]*
+🔥 *${prefix}rize [aleatórias]*
+🔥 *${prefix}minato [aleatórias]*
+🔥 *${prefix}boruto [aleatórias]*
+🔥 *${prefix}hinata [aleatórias]*
+🔥 *${prefix}sasuke [aleatórias]*
+🔥 *${prefix}sakura [aleatórias]*
+🔥 *${prefix}naruto [aleatórias]*
+🔥 *${prefix}meme*   
+🔥 *${prefix}lofi*
+🔥 *${prefix}malkova*
+🔥 *${prefix}canal*
+🔥 *${prefix}nsfwloli1*
+🔥 *${prefix}reislin*
+➛
+●ω●【 𝗜𝗡𝗧𝗘𝗟𝗜𝗚𝗘̂𝗡𝗖𝗜𝗔 𝗜𝗔 】
+➛
+🔥 *${prefix}simih 1 (para ativar)*
+🔥 *${prefix}simih 0 (para desativar)*
+🔥 *${prefix}simi (sua mensagem)*
+➛
+●ω●【𝗘𝗠 𝗧𝗘𝗦𝗧𝗘】
+🔥
+🔥 *${prefix}*
+🔥 *${prefix}*
+🔥 *${prefix}*
+➛
+●ω●【𝗣𝗥𝗘𝗠𝗜𝗨𝗠】
+➛
+🔥 *${prefix}dado*
+🔥 *${prefix}cekvip*
+🔥 *${prefix}premiumlist*
+🔥 *${prefix}delete*
+🔥 *${prefix}modapk*
+🔥 *${prefix}indo10*
+🔥 *${prefix}daftarvip [para virar Premium]*
+🔥 *${prefix}qrcode*
+🔥 *${prefix}chentai*
+🔥 *${prefix}gcpf*
+🔥 *${prefix}gbin*
+🔥 *${prefix}pack*
+🔥 *${prefix}destrava*
+🔥 *${prefix}gpessoa*
+
+●ω●【𝗚𝗥𝗨𝗣𝗢】
+➛
+🔥 *${prefix}banir*
+🔥 *${prefix}leveling [on/off]*
+🔥 *${prefix}level*
+🔥 *${prefix}add*
+🔥 *${prefix}promover*
+🔥 *${prefix}setfoto [na legenda]*
+🔥 *${prefix}setname [texto]*
+🔥 *${prefix}rebaixar*
+🔥 *${prefix}admins*
+🔥 *${prefix}marcar*
+🔥 *${prefix}marcar2*
+🔥 *${prefix}marcar3*
+🔥 *${prefix}bemvindo [1/0]*
+🔥 *${prefix}grupoinfo*
+🔥 *${prefix}bomdia*
+🔥 *${prefix}boatarde*
+🔥 *${prefix}boanoite*
+🔥 *${prefix}setdesc*
+🔥 *${prefix}bug [sua mensagem]*
+➛
+●ω●【𝗘𝗦𝗣𝗘𝗖𝗜𝗙𝗜𝗖𝗢 𝗗𝗢 𝗕𝗢𝗧】𝗹
+➛
+🔥 *${prefix}bug [sua mensagem]*
+🔥 *${prefix}clonar [@]*
+🔥 *${prefix}dono*
+🔥 *${prefix}ping [ver velocidade do bot]*
+🔥 *${prefix}termux*
+🔥 *${prefix}gay [@]*
+🔥 *${prefix}wame*
+🔥 *${prefix}map (nome)*
+🔥 *${prefix}setppbot (marque uma img)*
+🔥 *${prefix}pinterest (nome)*
+🔥 *${prefix}desligar (so para o dono)*
+🔥 *${prefix}timer*
+➛
+●ω●【𝗠𝗔𝗜𝗦 𝗔𝗟𝗚𝗨𝗡𝗦】𝗹
+➛
+🔥 *${prefix}neko*
+🔥 *${prefix}ttp [texto]*
+🔥 *${prefix}testime*
+🔥 *${prefix}tomp3*
+🔥 *${prefix}modoanime [on/off]*
+🔥 *${prefix}modonsfw [on/off]*
+🔥 *${prefix}happymod [jogo/app]*
+🔥 *${prefix}rize*
+🔥 *${prefix}ytsearch*
+🔥 *${prefix}moddroid [jogo/app]*
+🔥 *${prefix}xvideos [titulo]**
+🔥 *${prefix}nomegp*
+🔥 *${prefix}nabutojokes (memes aleatórios)*
+🔥 *${prefix}animecry*
+🔥 *${prefix}gay1*
+🔥 *${prefix}next*
+🔥 *${prefix}alerta*
+🔥 *${prefix}belle [img aleatórias]*
+🔥 *${prefix}pronomeneu [texto]*
+🔥 *${prefix}hobby*
+🔥 *𝗡𝗢𝗠𝗘: Takamura
+🔥 *𝗪𝗣𝗣: wa.me/+558695023910
+●ω●【 𝗖𝗢𝗠𝗔𝗡𝗗𝗢𝗦 𝗗𝗘 𝗠𝗨𝗦𝗜𝗖𝗔 】
+
+🔥 *${prefix} em teste
+🔥 *${prefix}jogaroxo*
+🔥 *${prefix} em teste
+🔥 *${prefix} em teste
+🔥 *${prefix}narutinho*
+🔥 *${prefix}}tobi*
+🔥 *${prefix}rapL*
+🔥 *${prefix}paypal*
+🔥 *${prefix}sad*
+
+●ω●【 𝗢𝗨𝗧𝗥𝗢𝗦 /2 】
+
+*🔥 *${prefix}antilink [1/0]*
+*🔥 *${prefix}brainly [pergunta]*
+🔥 *${prefix}antiracismo [on/off]*
+🔥 *${prefix}setnomebot*
+🔥 *${prefix}meme*
+
+●ω●【 𝗜𝗡𝗧𝗘𝗥𝗔𝗧𝗜𝗩𝗢𝗦 】
+
+NOTA »
+Mandar a msg sem o prefixo
+
+
+🔥 *${prefix} *beat1*
+🔥 *${prefix} *beat2*
+🔥 *${prefix} *hentaisom* (erro)`
+}
+
+exports.help = help
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
